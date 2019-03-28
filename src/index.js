34ed83b333b4e4ca400ea77a2ce2ce4fc7f3abfb
Drew Verwiel <Verwiel.Code@gmail.com>
2019-03-28 15:37:26 -0600

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,128 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+//children
+  function Square(props) {
+    return (
+      <button className="square" onClick={props.onClick}>
+        {props.value}
+      </button>
+    );
+  }
+//parent
+  class Board extends React.Component {
+    constructor(props) { 
+//used to initialize the this.state 
+      super(props);
+//always need a super after constructor
+      this.state = {
+        squares: Array(9).fill(null), 
+//defaults array to all null
+        xIsNext: true,
+      };
+    }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+    handleClick(i) { 
+//defined for renderSquare onClick below
+      const squares = this.state.squares.slice(); 
+//slice creates copy of array to modify
+      if (calculateWinner(squares) || squares[i]) {
+        return;
+      }
+//keeps user from entering in a taken spot or if game is over
+      squares[i] = this.state.xIsNext ? 'X' : 'O'; 
+//gives value to square clicked
+      this.setState({
+        squares: squares,
+        xIsNext: !this.state.xIsNext,
+      });
+    }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    renderSquare(i) {
+      return ( 
+//parentheses to prevent ; after return since its split
+        <Square
+          value = {this.state.squares[i]} 
+//each square now gets the prop from squares
+          onClick={() => this.handleClick(i)} 
+//called by square
+        />
+      );
+    }
+  
+    render() {
+      const winner = 
+    calculateWinner(this.state.squares);
+      let status;
+      if (winner) {
+        status = 'Winner: ' + winner;
+      } else {
+        status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+      }
+//declares winner or which player is next on top of the board
+      return (
+        <div>
+          <div className="status">{status}</div>
+          <div className="board-row">
+            {this.renderSquare(0)}
+            {this.renderSquare(1)}
+            {this.renderSquare(2)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(3)}
+            {this.renderSquare(4)}
+            {this.renderSquare(5)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(6)}
+            {this.renderSquare(7)}
+            {this.renderSquare(8)}
+          </div>
+        </div>
+      );
+    }
+  }
+  
+  class Game extends React.Component {
+    render() {
+      return (
+        <div className="game">
+          <div className="game-board">
+            <Board />
+          </div>
+          <div className="game-info">
+            <div>{/* status */}</div>
+            <ol>{/* TODO */}</ol>
+          </div>
+        </div>
+      );
+    }
+  }
+
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+  
+  // ========================================
+  
+  ReactDOM.render(
+    <Game />,
+    document.getElementById('root')
+  );
+  
